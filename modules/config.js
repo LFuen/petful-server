703c1ef04d84c0bde81872dec0d44c67a83ff7a6
Lili Fuentes <lile7886@gmail.com>
2021-01-21 15:54:51 -0500

    client origin fix

--- modules/config.js
+++ modules/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://compassionate-swartz-232054.netlify.app',
+  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://upbeat-lamport-5c1c9d.netlify.app',
   PORT : process.env.PORT || 8000
 }
