f06a14fa1158ee97a298ed70ffc0ac9d0fafb4df
Lili Fuentes <lile7886@gmail.com>
2021-01-21 11:50:17 -0500

    client origin change

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require('./modules/app/app');
 const { PORT } = require('./modules/config');
-const port = process.env.PORT || 8080
-
+const port = process.env.PORT || 8000
 app.listen(port, () => {
   console.log(`Server is live on ${ PORT }.`)
 })
